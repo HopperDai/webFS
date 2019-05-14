c5a9ec923db937b7204b2e6094850bec69091465
hopper <hopper@hopper-3.local>
2019-05-14 13:29:08 +0800

    seajs

--- module/原生js实现/my_seajs/my_sea.js
+++ module/原生js实现/my_seajs/my_sea.js
@@ -9,7 +9,9 @@ const seajs = {
             success(m_code) {
                 // m_code 读取到的模块的代码
 
-                // 模块的代码标准为：define(cb) ，即执行 define ，因此此处应该定义一个 define 函数
+                // 模块的代码格式为：define(cb) ;
+                // 通过 eval 方法执行读取 path 路径文件的代码字符串；
+                // 定义 define 函数，把初始化的参数传递给模块；把模块导出的数据导入到 use 方法的回调中
                 function define(fn) {
                     /* 
                         调用执行模块里的回调
@@ -20,7 +22,7 @@ const seajs = {
                         exports: {}
                     }
 
-                    fn(function () {}, module.exports, module);
+                    fn(function () {}, module.exports, module); // 把初始化的参数传递给模块
 
                     // 把获取到的 module 输出给最外层的回调
                     fn_end(module.exports);
