357b3cfe6996cf5b14e463bdb126610ba25ad5c0
hopper <hoppergoo@gmail.com>
2019-05-16 00:06:53 +0800

    bugfix: 读取模块中内嵌的其它模块的地址

--- module/原生js实现/my_seajs/my_sea.js
+++ module/原生js实现/my_seajs/my_sea.js
@@ -13,27 +13,25 @@ const seajs = {
                 // 通过 eval 方法执行读取 path 路径文件的代码字符串；
                 // 定义 define 函数，把初始化的参数传递给模块；把模块导出的数据导入到 use 方法的回调中
                 function define(fn) {
-                    /* 
-                        调用执行模块里的回调
-                        fn 的三个参数：require、exports、module
-                    */
-
+                    // 执行模块的回调。fn 的参数为 require, exports, module
                     let module = {
                         exports: {}
                     }
+                    fn(function () {}, module.exports, module);
 
-                    fn(function () {}, module.exports, module); // 把初始化的参数传递给模块
+                    // 步骤一：读取模块中嵌入的其它模块文件。即找出 require 引用的文件
+                    const temp = fn.toString();
+                    const start = temp.indexOf('{') + 1;
+                    const end = temp.indexOf('}');
+                    const fnBody = temp.substring(start, end); // 函数体
+                    // 模块路径列表
+                    const pathArr = fnBody.match(/require\([^\(\)]+\)/g).map(item => {
+                        return item.substring(item.search(/\"|\'/)+1, item.search(/[\'|\"]\)$/));
+                    });
 
                     // 把获取到的 module 输出给最外层的回调
                     fn_end(module.exports);
                 }
-
-                // 第一步，把 require 都找出来
-                let tmp = m_code.substring(m_code.indexOf('{') + 1, m_code.lastIndexOf('}'));
-                let modArr = tmp.match(/require\([^\(\)]+\)/g).map(item => {
-                    return item.substring(item.search(/\"|\'/) + 1, item.search(/[\'|\"]\)$/));
-                });
-                console.log(modArr);
 
                 // 第二步，使用 eval 运行模块的代码
                 eval(m_code);
