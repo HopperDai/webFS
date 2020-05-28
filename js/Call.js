21364b03086cd3bb80d9c56afe99c5d819213e55
hopper <hoppergoo@gmail.com>
2020-05-28 11:43:37 +0800

    feat: 简写 call

---
+++ js/Call.js
@@ -0,0 +1,8 @@
+Function.prototype.call2 = function(context) {
+  const ctx = context || window // context 参数可能为 null
+  const [, ...args] = Array.from(arguments) // 获取参数，剔除第一个 context 参数
+  ctx.fn = this // this 指向调用 call2 的实例
+  const res = eval(`ctx.fn(${args.join(',')})`)
+  delete ctx.fn
+  return res // 函数可能有 return
+}
